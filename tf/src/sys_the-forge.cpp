fe43124dce6dbbe7f1b392370ef7d41b36dd27df
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2024-02-21 04:47:12 +0700

    add some Init() and Exit() code

--- tf/src/sys_the-forge.cpp
+++ tf/src/sys_the-forge.cpp
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
 #include <IApp.h>
+#include <IGraphics.h>
+#include <IInput.h>
 #include <ILog.h>
+#include <RingBuffer.h>
+
+Renderer *pRenderer = NULL;
+
+Queue *pGraphicsQueue = NULL;
+GpuCmdRing gGraphicsCmdRing = {};
+
+SwapChain *pSwapChain = NULL;
+RenderTarget *pDepthBuffer = NULL;
+Semaphore *pImageAcquiredSemaphore = NULL;
+
+const uint32_t gDataBufferCount = 2;
 
 class MainApp : public IApp
 {
-public:
+  public:
     bool Init() override;
     void Exit() override;
     bool Load(ReloadDesc *pReloadDesc) override;
@@ -15,17 +29,73 @@ public:
     const char *GetName() override;
 };
 
-const char *MainApp::GetName() { return "tfQuake2"; }
+const char *MainApp::GetName()
+{
+    return "tfQuake2";
+}
 
 bool MainApp::Init()
-{ 
-    LOGF(LogLevel::eDEBUG, "Hello World");
+{
+    // FILE PATHS
+    fsSetPathForResourceDir(pSystemFileIO, RM_CONTENT, RD_SHADER_BINARIES, "CompiledShaders");
+    fsSetPathForResourceDir(pSystemFileIO, RM_CONTENT, RD_GPU_CONFIG, "GPUCfg");
+    fsSetPathForResourceDir(pSystemFileIO, RM_CONTENT, RD_TEXTURES, "Textures");
+    fsSetPathForResourceDir(pSystemFileIO, RM_CONTENT, RD_FONTS, "Fonts");
+    fsSetPathForResourceDir(pSystemFileIO, RM_DEBUG, RD_SCREENSHOTS, "Screenshots");
+    fsSetPathForResourceDir(pSystemFileIO, RM_CONTENT, RD_SCRIPTS, "Scripts");
+    fsSetPathForResourceDir(pSystemFileIO, RM_DEBUG, RD_DEBUG, "Debug");
+
+    // window and renderer setup
+    RendererDesc settings = {
+        .mD3D11Supported = false,
+        .mGLESSupported = false,
+    };
+
+    initRenderer(GetName(), &settings, &pRenderer);
+    // check for init success
+    if (!pRenderer)
+        return false;
+
+    QueueDesc queueDesc = {};
+    queueDesc.mType = QUEUE_TYPE_GRAPHICS;
+    queueDesc.mFlag = QUEUE_FLAG_INIT_MICROPROFILE;
+    addQueue(pRenderer, &queueDesc, &pGraphicsQueue);
+
+    GpuCmdRingDesc cmdRingDesc = {};
+    cmdRingDesc.pQueue = pGraphicsQueue;
+    cmdRingDesc.mPoolCount = gDataBufferCount;
+    cmdRingDesc.mCmdPerPoolCount = 1;
+    cmdRingDesc.mAddSyncPrimitives = true;
+    addGpuCmdRing(pRenderer, &cmdRingDesc, &gGraphicsCmdRing);
+
+    addSemaphore(pRenderer, &pImageAcquiredSemaphore);
+
+    initResourceLoaderInterface(pRenderer);
+
+    InputSystemDesc inputDesc = {
+        .pRenderer = pRenderer,
+        .pWindow = pWindow,
+    };
+
+    if (!initInputSystem(&inputDesc))
+        return false;
+
     return true;
 }
 
 void MainApp::Exit()
 {
-    LOGF(LogLevel::eDEBUG, "Good bye!");
+    exitInputSystem();
+    
+    removeGpuCmdRing(pRenderer, &gGraphicsCmdRing);
+    removeSemaphore(pRenderer, pImageAcquiredSemaphore);
+
+    exitResourceLoaderInterface(pRenderer);
+
+    removeQueue(pRenderer, pGraphicsQueue);
+
+    exitRenderer(pRenderer);
+    pRenderer = NULL;
 }
 
 bool MainApp::Load(ReloadDesc *pReloadDesc)
