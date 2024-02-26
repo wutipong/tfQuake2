0a573e1642bbd03cfadaf757d9683746f4474e47
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2024-02-27 05:30:14 +0700

    add cd volume control.

--- tf/src/cd_soloud.cpp
+++ tf/src/cd_soloud.cpp
@@ -12,6 +12,11 @@ extern "C"
 {
 #include "../../client/client.h"
 
+    static cvar_t *cd_volume;
+    static cvar_t *cd_loopcount;
+    static cvar_t *cd_looptrack;
+    static cvar_t *no_music;
+    
     qboolean CDAudio_Play(int track, qboolean looping)
     {
         gSoloud.stop(handle);
@@ -22,6 +27,7 @@ extern "C"
         stream.load(path.c_str());
         handle = gSoloud.playBackground(stream);
         gSoloud.setLooping(handle, looping);
+        gSoloud.setVolume(handle, cd_volume->value);
 
         return false;
     }
@@ -29,7 +35,8 @@ extern "C"
     void CDAudio_Stop(void)
     {
         LOGF(LogLevel::eINFO, "Stop music");
-        if(handle == -1) return;
+        if (handle == -1)
+            return;
         gSoloud.setPause(handle, true);
     }
 
@@ -41,12 +48,22 @@ extern "C"
 
     void CDAudio_Update(void)
     {
+        if (cd_volume->modified)
+        {
+            cd_volume->modified = false;
+            gSoloud.setVolume(handle, cd_volume->value);
+        }
     }
 
     int CDAudio_Init(void)
     {
         LOGF(LogLevel::eINFO, "Init music");
-        
+
+        cd_volume = Cvar_Get(std::string("cd_volume").data(), std::string("1").data(), CVAR_ARCHIVE);
+        cd_loopcount = Cvar_Get(std::string("cd_loopcount").data(), std::string("4").data(), 0);
+        cd_looptrack = Cvar_Get(std::string("cd_looptrack").data(), std::string("11").data(), 0);
+        no_music = Cvar_Get(std::string("no_music").data(), std::string("0").data(), 0);
+
         return 0;
     }
 
